1c44b8ae4215e9860fbadd9626f702a59f5d4a90
Stepan Prokofjev <Kolbasko111@yandex.ru>
2015-06-02 01:06:38 +0300

    [WP] CMakeLists.txt progress

---
+++ luna2d/platform/wp/application/HelloTriangleRenderer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pch.h"
+
+namespace WpTestApp
+{
+    class HelloTriangleRenderer
+    {
+    public:
+        HelloTriangleRenderer();
+        ~HelloTriangleRenderer();
+        void Draw(GLsizei width, GLsizei height);
+    };
+}
