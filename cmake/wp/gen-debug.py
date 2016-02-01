273272810cf0165e1a624784af7752a037327b0f
Stepan Prokofjev <Kolbasko111@yandex.ru>
2016-02-01 03:14:13 +0300

    Move gen-wp.py to separate folder

---
+++ cmake/wp/gen-debug.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.Popen("gen.py Debug", shell=True)
--- cmake/wp/gen.py
+++ cmake/wp/gen.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import sys
 
-LUNA2D_PATH = os.path.realpath(__file__) + "/../../"
+LUNA2D_PATH = os.path.realpath(__file__) + "/../../../"
 VS_VERSION = "Visual Studio 12 2013 ARM"
 BUILD_TYPE = sys.argv[1] if len(sys.argv) > 1 else "Release"
 BUILD_PATH = LUNA2D_PATH + "/build/wp/" + BUILD_TYPE.lower()
