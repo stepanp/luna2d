a3f18ac8a47cb0840b79b21d02939e327846ddfb
Stepan Prokofjev <Kolbasko111@yandex.ru>
2017-06-15 01:43:43 +0300

    [iOS][Services] Notifications for iOS 10+

--- luna2d/services/platform/ios/lunaiosnotifications.mm
+++ luna2d/services/platform/ios/lunaiosnotifications.mm
@@ -25,6 +25,7 @@
 #include "lunansstring.h"
 #include "lunalog.h"
 #include "lunamacro.h"
+#import <UserNotifications/UserNotifications.h>
 
 using namespace luna2d;
 
@@ -33,29 +34,40 @@ using namespace luna2d;
 //---------------------------------------------------
 @interface Notifications10 : NSObject
 
+-(void) requestAccess;
+
 -(void) schedule: (NSString*)message secondsFromNow: (int)secondsFromNow;
 
 -(void) cancel;
-
--(void) register;
 
 @end
 
 @implementation Notifications10
 
+-(void) requestAccess
+{
+	UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+	[center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert) completionHandler:nil];
+}
+
 -(void) schedule: (NSString*)message secondsFromNow: (int)secondsFromNow
 {
+	UNMutableNotificationContent* notification = [[UNMutableNotificationContent alloc] init];
+    notification.body = message;
+    notification.sound = [UNNotificationSound defaultSound];
+    notification.badge = [NSNumber numberWithInt:1];
 
+    UNTimeIntervalNotificationTrigger* trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:secondsFromNow repeats:NO];
+    UNNotificationRequest* request = [UNNotificationRequest requestWithIdentifier:@"0" content:notification trigger:trigger];
+    
+    UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+    [center addNotificationRequest:request withCompletionHandler:nil];
 }
 
 -(void) cancel
 {
-
-}
-
--(void) register
-{
-
+	UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+	[center removeAllPendingNotificationRequests];
 }
 	
 @end
@@ -66,15 +78,21 @@ using namespace luna2d;
 //-------------------------------------------------
 @interface Notifications89 : NSObject
 
+-(void) requestAccess;
+
 -(void) schedule: (NSString*)message secondsFromNow: (int)secondsFromNow;
 
 -(void) cancel;
-
--(void) register;
 	
 @end
 
 @implementation Notifications89
+
+-(void) requestAccess
+{
+	UIUserNotificationSettings* settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeAlert | UIUserNotificationTypeSound categories:nil];
+	[[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+}
 
 -(void) schedule: (NSString*)message secondsFromNow: (int)secondsFromNow
 {
@@ -97,12 +115,6 @@ using namespace luna2d;
 		 [[UIApplication sharedApplication] cancelLocalNotification:notification];
 	}
 }
-
--(void) register
-{
-	UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeAlert | UIUserNotificationTypeSound categories:nil];
-	[[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-}
 	
 @end
 
@@ -114,8 +126,8 @@ LUNAIosNotifications::LUNAIosNotifications() : LUNANotifications()
 	if([[[UIDevice currentDevice] systemVersion] floatValue] < 10.0) impl = [[Notifications89 alloc] init];
 	else impl = [[Notifications10 alloc] init];
 
-	[impl register];
 	[UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+	[impl requestAccess];
 }
 
 // Schedule local push notification
